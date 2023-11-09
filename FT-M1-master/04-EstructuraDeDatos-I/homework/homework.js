d21f6e803977f74bad3404ccc5ba6da9416f9a98
JeronimoRoss <jero.ross@gmail.com>
2023-11-09 18:33:43 -0300

    Homework resuelta del dia 09/11/2023 - Jeronimo Ross

--- FT-M1-master/04-EstructuraDeDatos-I/homework/homework.js
+++ FT-M1-master/04-EstructuraDeDatos-I/homework/homework.js
@@ -14,9 +14,64 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * nFactorial(n - 1);
+  }
+}
 
-function nFibonacci(n) {}
+console.log(nFactorial(5));
+
+
+
+function nFibonacci(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return nFibonacci(n - 1) + nFibonacci(n - 2);
+  }
+}
+
+console.log(nFibonacci(7));
+
+// FUNCIONES ITERATIVAS
+
+function factorialIterativo(n) {
+  if (n === 0) {
+    return 1;
+  }
+  let resultado = 1;
+  for (let i = 1; i <= n; i++) {
+    resultado *= i;
+  }
+  return resultado;
+}
+
+console.log(factorialIterativo(5));
+
+
+
+function fibonacciIterativo(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  let prev = 0;
+  let curr = 1;
+  for (let i = 2; i <= n; i++) {
+    let temp = curr;
+    curr = prev + curr;
+    prev = temp;
+  }
+  return curr;
+}
+console.log(fibonacciIterativo(7));
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -27,7 +82,30 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+function Queue() {
+  this.elements = [];
+}
+
+Queue.prototype.enqueue = function(value) {
+  this.elements.push(value);
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.elements.length === 0) {
+    return undefined;
+  }
+  return this.elements.shift();
+};
+
+Queue.prototype.size = function() {
+  return this.elements.length;
+};
+let myQueue = new Queue();
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+console.log(myQueue.dequeue());
+console.log(myQueue.size());
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
