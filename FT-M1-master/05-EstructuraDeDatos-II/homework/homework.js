bb7182d6db8a3d2c058883eadd2e29d0c0e0e197
JeronimoRoss <jero.ross@gmail.com>
2023-11-13 22:13:38 -0300

    Homework resuelto del día viernes 10/11/2023 - Jerónimo Ross

--- FT-M1-master/05-EstructuraDeDatos-II/homework/homework.js
+++ FT-M1-master/05-EstructuraDeDatos-II/homework/homework.js
@@ -10,9 +10,86 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
-function LinkedList() {}
+function Node(value) {
+  return {
+    value: value,
+    next: null,
+  };
+}
 
-function Node(value) {}
+function LinkedList() {
+  const list = {
+    head: null,
+  };
+
+  function add(value) {
+    const newNode = Node(value);
+    if (!list.head) {
+      list.head = newNode;
+    } else {
+      let current = list.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  function remove() {
+    if (!list.head) {
+      return null;
+    }
+
+    let removedValue;
+    if (!list.head.next) {
+      // Caso particular: solo hay un nodo en la lista
+      removedValue = list.head.value;
+      list.head = null;
+    } else {
+      let current = list.head;
+      while (current.next && current.next.next) {
+        current = current.next;
+      }
+      removedValue = current.next.value;
+      current.next = null;
+    }
+
+    return removedValue;
+  }
+
+  function search(param) {
+    if (!list.head) {
+      return null;
+    }
+
+    let current = list.head;
+    while (current) {
+      if (typeof param === 'function' && param(current.value)) {
+        return current.value;
+      } else if (param === current.value) {
+        return current.value;
+      }
+      current = current.next;
+    }
+
+    return null;
+  }
+
+  return {
+    add,
+    remove,
+    search,
+  };
+}
+
+const linkedList = LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+
+console.log(linkedList.search(2)); // Retorna 2
+console.log(linkedList.search(value => value % 2 === 0)); // Retorna 2 (número par)
+console.log(linkedList.remove()); // Elimina el último nodo (con valor 3) y retorna 3
 
 /* EJERCICIO 2
 Implementar la clase HashTable.
@@ -27,7 +104,79 @@ La clase debe tener los siguientes métodos:
 
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
-function HashTable() {}
+function createHashTable() {
+  const numBuckets = 35;
+  const buckets = new Array(numBuckets).fill(null).map(() => []);
+
+  function hash(key) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+      total += key.charCodeAt(i);
+    }
+    return total % numBuckets;
+  }
+
+  function set(key, value) {
+    const index = hash(key);
+    const bucket = buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      const [storedKey, storedValue] = bucket[i];
+      if (storedKey === key) {
+        // La clave ya existe, actualizamos el valor
+        bucket[i] = [key, value];
+        return;
+      }
+    }
+
+    // La clave no existe, la agregamos al bucket
+    bucket.push([key, value]);
+  }
+
+  function get(key) {
+    const index = hash(key);
+    const bucket = buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      const [storedKey, storedValue] = bucket[i];
+      if (storedKey === key) {
+        // La clave existe, retornamos el valor asociado
+        return storedValue;
+      }
+    }
+
+    // La clave no existe en la tabla
+    return undefined;
+  }
+
+  function hasKey(key) {
+    const index = hash(key);
+    const bucket = buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      const [storedKey, storedValue] = bucket[i];
+      if (storedKey === key) {
+        // La clave existe en la tabla
+        return true;
+      }
+    }
+
+    // La clave no existe en la tabla
+    return false;
+  }
+
+  return {
+    set,
+    get,
+    hasKey,
+  };
+}
+
+const myHashTable = createHashTable();
+myHashTable.set('instructora', 'Ani');
+console.log(myHashTable.hasKey('instructora')); // Retorna true
+console.log(myHashTable.get('instructora')); // Retorna 'Ani'
+console.log(myHashTable.hasKey('otraClave')); // Retorna false
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
