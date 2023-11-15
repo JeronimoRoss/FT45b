9bc8fc560d1edd4366cb0ae488f41122447b4393
JeronimoRoss <jero.ross@gmail.com>
2023-11-15 19:07:05 -0300

    Homework resuelto del día miercoles 15/11/2023 - Jerónimo Ross

--- FT-M1-master/08-Algoritmos-II/homework/homework.js
+++ FT-M1-master/08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,29 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  function quickSort(array) {
+    if (array.length <= 1) {
+      return array;
+    }
+  
+    const pivot = array[array.length - 1];
+    const left = [];
+    const right = [];
+  
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] < pivot) {
+        left.push(array[i]);
+      } else {
+        right.push(array[i]);
+      }
+    }
+  
+    return [...quickSort(left), pivot, ...quickSort(right)];
+  }
+  const arrayDesordenado = [3, -1, -24, 539, 790, 80, 1];
+const arrayOrdenado = quickSort(arrayDesordenado.slice());
+console.log(arrayOrdenado);
+
 }
 
 function mergeSort(array) {
@@ -14,6 +37,39 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+
+  function mergeSort(array) {
+    if (array.length <= 1) {
+      return array;
+    }
+  
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+  
+    return merge(mergeSort(left), mergeSort(right));
+  }
+  
+  function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+  
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        result.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+  
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+  }
+  const arrayDesordenado = [3, -10, -591, 4, 2, 180, 10];
+const arrayOrdenado = mergeSort(arrayDesordenado.slice());
+console.log(arrayOrdenado);
 
   //okey probemos ahora
 }
